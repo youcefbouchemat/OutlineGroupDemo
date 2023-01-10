f7fb7d9d9371dc9b97208593d9ed9c349951b612
apple <apple@MacBook-Pro.local>
2023-01-10 20:32:06 +0100

    initial commit

---
+++ OutlineGroupDemo/OutlineGroupDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  OutlineGroupDemoApp.swift
+//  OutlineGroupDemo
+//
+//  Created by apple on 10/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct OutlineGroupDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
